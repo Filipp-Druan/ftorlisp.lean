9dee06d01b25eb17a674cd9ddd392cd8ce86d6b0
Filipp Druan <druanf@bk.ru>
2026-07-09 22:56:45 +0300

    Добавил метод toInt. Кажется, не работает.

--- Ftorlisp/UnTyAST.lean
+++ Ftorlisp/UnTyAST.lean
@@ -2,16 +2,21 @@ namespace Ftorlisp.UnTyAST
 
 mutual
   inductive UnTyExprAST where
-  | add (list : List UnTyAST)
-  | mul (list : List UnTyAST)
-  | sub (list : List UnTyAST)
-  | div (list : List UnTyAST)
+    | add (list : List UnTyAST)
+    | mul (list : List UnTyAST)
+    | sub (list : List UnTyAST)
+    | div (list : List UnTyAST)
   deriving Nonempty, Repr, BEq
 
   inductive UnTyAST where
-  | intLit (val : Int)
-  | sym (name : String)
-  | exp (val : UnTyExprAST)
-  | let_statement (name : UnTyAST) (val : UnTyAST) -- name : UnTyAST.sym
+    | intLit (val : Int)
+    | sym (name : String)
+    | exp (val : UnTyExprAST)
+    | let_statement (name : UnTyAST) (val : UnTyAST) -- name : UnTyAST.sym
   deriving Nonempty, Repr, BEq
 end
+
+def toInt (ast : UnTyAST) : Option Int :=
+    match ast with
+      | .intLit val => .some val
+      | _ => .none
