126a82ed1102d3c7edcaabcf13bee8e0e1760c0b
Filipp Druan <druanf@bk.ru>
2026-07-09 20:56:55 +0300

    Вынес математические выражения в отдельный тип, Добавил выражение let

--- Ftorlisp/SecondParser.lean
+++ Ftorlisp/SecondParser.lean
@@ -16,6 +16,9 @@ inductive SecondParserError where
   | unknownOperator (parse_tree : ParseTree)
   | operatorNotSymbol (parse_tree : ParseTree)
   | notCallUnreachable
+  | letNot2Args (args : List ParseTree)
+  | letValNotExp (ast : UnTyAST)
+  | letNameNotSym
 deriving Nonempty, Repr, BEq
 
 mutual
@@ -34,21 +37,49 @@ mutual
               | "+" => do
                 let arg_ASTs ← List.mapM ast_parser arguments
 
-                return .add arg_ASTs
+                return .exp $ .add arg_ASTs
               | "*" => do
                 let arg_ASTs ← List.mapM ast_parser arguments
 
-                return .mul arg_ASTs
+                return .exp $ .mul arg_ASTs
               | "-" => do
                 let arg_ASTs ← List.mapM ast_parser arguments
 
-                return .sub arg_ASTs
+                return .exp $ .sub arg_ASTs
               | "/" => do
                 let arg_ASTs ← List.mapM ast_parser arguments
 
-                return .div arg_ASTs
+                return .exp $ .div arg_ASTs
+
+              | "let" => let_args_parser arguments
 
               | _ => .error (.unknownOperator operator)
           | _ => .error (.operatorNotSymbol operator)
       | _ => .error .notCallUnreachable
+
+  partial def let_name_parser (name : ParseTree) : Except SecondParserError UnTyAST := do
+    let name_ast ← ast_parser name
+
+    match name_ast with
+      | .sym _ => return name_ast
+      | _ => .error .letNameNotSym
+
+  partial def let_val_parser (val : ParseTree) : Except SecondParserError UnTyAST := do
+    let val_ast ← ast_parser val
+
+    match val_ast with
+      | .exp _ => return val_ast
+      | _ => .error $ .letValNotExp val_ast
+
+  partial def let_args_parser (args : List ParseTree) : Except SecondParserError UnTyAST :=
+    match args with
+      | [name, val] => do
+        let name_ast ← let_name_parser name
+        let val_ast ← let_val_parser val
+
+        return .let_statement name_ast val_ast
+      | _ => .error $ .letNot2Args args
+
+
+
 end
--- Ftorlisp/UnTyAST.lean
+++ Ftorlisp/UnTyAST.lean
@@ -1,10 +1,17 @@
 namespace Ftorlisp.UnTyAST
 
-inductive UnTyAST where
-  | intLit (val : Int)
-  | sym (name : String)
+mutual
+  inductive UnTyExprAST where
   | add (list : List UnTyAST)
   | mul (list : List UnTyAST)
   | sub (list : List UnTyAST)
   | div (list : List UnTyAST)
-deriving Nonempty, Repr, BEq
+  deriving Nonempty, Repr, BEq
+
+  inductive UnTyAST where
+  | intLit (val : Int)
+  | sym (name : String)
+  | exp (val : UnTyExprAST)
+  | let_statement (name : UnTyAST) (val : UnTyAST) -- name : UnTyAST.sym
+  deriving Nonempty, Repr, BEq
+end
