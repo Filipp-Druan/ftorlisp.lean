740ef19ebe1a28d565259377f2eed185b9db2480
Filipp Druan <druanf@bk.ru>
2026-07-13 13:24:01 +0300

    Вынес парсинг в отдельную функцию

--- Ftorlisp/FirstParser.lean
+++ Ftorlisp/FirstParser.lean
@@ -12,40 +12,47 @@ inductive FirstParserError where
   | int
   | sym
   | list
-deriving Repr
+deriving Inhabited, Repr
 
-def intParser : Parser FirstParserError ParseTree := do
+private def intParser : Parser FirstParserError ParseTree := do
   let minus ← maybe (char '-')
   let num ← wholeNumber
   match minus with
     | .some _ => return (.int (- (Int.ofNat num)))
     | .none => return .int $ Int.ofNat num
 
-def isMathChar (ch : Char) : Bool :=
+private def isMathChar (ch : Char) : Bool :=
   ch == '+' || ch == '-' || ch == '*' || ch == '/'
 
-def isSymbolChar (ch : Char) : Bool :=
+private def isSymbolChar (ch : Char) : Bool :=
   ch.isAlphanum || isMathChar ch
 
-def symParser : Parser FirstParserError ParseTree := do
+private def symParser : Parser FirstParserError ParseTree := do
   let chars ← many1 (sat isSymbolChar)
   return .sym (String.ofList chars.toList)
 
 mutual
-  partial def listParser : Parser FirstParserError ParseTree := do
+  private partial def listParser : Parser FirstParserError ParseTree := do
     let _ ←  char '('
-    let exprs ← sepBy exprFirstParser ws
+    let exprs ← sepBy exprParser ws
     let _ ← char ')'
     return .call exprs.toList
 
-  partial def exprFirstParser : Parser FirstParserError ParseTree := do
+  private partial def exprParser : Parser FirstParserError ParseTree := do
     (withErr (.custom FirstParserError.int)  intParser) <|>
     (withErr (.custom FirstParserError.sym)  symParser) <|>
     (withErr (.custom FirstParserError.list) listParser)
 end
 
+partial def exprFirstParser (src : String) : Except FirstParserError ParseTree :=
+  match exprParser ⟨src, 0⟩ with
+    | .error err => match err.err with
+      | .custom fperr => .error fperr
+      | _ => unreachable!
+    | .ok parser_res => .ok parser_res.val
+
 partial def programFirstParser : Parser FirstParserError (List ParseTree) := do
-  let arr ← many exprFirstParser
+  let arr ← many exprParser
   return arr.toList
 
-#eval (exprFirstParser (ParserState.mk "(+ 1 2 (* 3 4))" 0))
+#eval (exprFirstParser "(+ 1 2 (* 3 4))")
