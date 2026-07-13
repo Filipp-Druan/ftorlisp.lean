7edf32d1dc1c7cefb3c2a47b083b4b807b27768d
Filipp Druan <druanf@bk.ru>
2026-07-13 12:24:58 +0300

    Чего-то сделал

--- Ftorlisp/TyCheck.lean
+++ Ftorlisp/TyCheck.lean
@@ -83,32 +83,31 @@ inductive TyInfRes (α : Type) where
 deriving Repr, BEq
 
 mutual
+  private partial def expTyInference (exp : UnTyASTExpr) (ty_table : TyTable) (env : Environment) : TyInfExcept TyASTExpr :=
+    match exp with
+      | .intLit val => .ok $ .int ty_table.int val
+      | .bool val => .ok $ .bool ty_table.bool val
+      | .sym name => match (env.lookup name) with
+        | .some ty => .ok $ .varRead ty name
+        | .none => .error .undefinedVar
+      | .binOp op arg1 arg2 => do
+        let arg1_ast ← expTyInference arg1 ty_table env
+        let arg2_ast ← expTyInference arg2 ty_table env
 
-partial def expTyInference (exp : UnTyASTExpr) (ty_table : TyTable) (env : Environment) : TyInfExcept TyASTExpr :=
-  match exp with
-    | .intLit val => .ok $ .int ty_table.int val
-    | .bool val => .ok $ .bool ty_table.bool val
-    | .sym name => match (env.lookup name) with
-      | .some ty => .ok $ .varRead ty name
-      | .none => .error .undefinedVar
-    | .binOp op arg1 arg2 => do
-      let arg1_ast ← expTyInference arg1 ty_table env
-      let arg2_ast ← expTyInference arg2 ty_table env
+        if arg1_ast.ty == arg2_ast.ty && arg1_ast.ty == ty_table.int then
+          return .binOp arg1_ast.ty op arg1_ast arg2_ast
+        else
+          .error $ .arithArgsTypeMismatch arg1_ast arg2_ast
 
-      if arg1_ast.ty == arg2_ast.ty && arg1_ast.ty == ty_table.int then
-        return .binOp arg1_ast.ty op arg1_ast arg2_ast
-      else
-        .error $ .arithArgsTypeMismatch arg1_ast arg2_ast
+      | .unOp .neg arg => do
+        let arg_ast ← expTyInference arg ty_table env
 
-    | .unOp .neg arg => do
-      let arg_ast ← expTyInference arg ty_table env
+        if arg_ast.ty == ty_table.int then
+          return (.unOp arg_ast.ty .neg arg_ast)
+        else
+          .error $ .negNotNum arg_ast
 
-      if arg_ast.ty == ty_table.int then
-        return (.unOp arg_ast.ty .neg arg_ast)
-      else
-        .error $ .negNotNum arg_ast
-
-  partial def stmtTyInference (stmt : UnTyASTStmt) (ty_table : TyTable) (env : Environment) : TyInfExcept TyASTStmt := do
+  private partial def stmtTyInference (stmt : UnTyASTStmt) (ty_table : TyTable) (env : Environment) : TyInfExcept TyASTStmt := do
     match stmt with
       | .let_stmt name val => do
         let val_ast ← expTyInference val ty_table env
