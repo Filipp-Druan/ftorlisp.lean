269c062ef934d3eebed30c4c874df787d48af5e3
Filipp Druan <druanf@bk.ru>
2026-07-14 17:05:37 +0300

    Поределил Fn

---
+++ Ftorlisp/Fn.lean
@@ -0,0 +1,24 @@
+import Ftorlisp.Ty
+import Ftorlisp.TyAST
+
+open Ftorlisp.Ty
+open Ftorlisp.TyAST
+
+namespace Ftorlisp.Fn
+
+structure FnDef where
+  ast : List TyAST
+deriving Repr, BEq
+
+structure Fn where
+  dec : Ty
+  definition : Option FnDef
+deriving Repr, BEq
+
+namespace Fn
+  def makeFromDecTy (dec_ty : Ty) : Fn :=
+    ⟨dec_ty, .none⟩
+
+  def addDef (fn : Fn) (fn_def : FnDef) : Fn :=
+    {fn with definition := .some fn_def}
+end Fn
