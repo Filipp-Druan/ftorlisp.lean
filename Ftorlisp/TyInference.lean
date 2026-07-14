207d278dc946229c6545a6d56be549bfedd41ca5
Filipp Druan <druanf@bk.ru>
2026-07-14 14:19:54 +0300

    Много чего сделал

--- Ftorlisp/TyInference.lean
+++ Ftorlisp/TyInference.lean
@@ -1,62 +1,14 @@
 import Ftorlisp.UnTyAST
 import Ftorlisp.OpTypes
-import Std.Data.HashMap
+import Ftorlisp.Ty
+import Ftorlisp.Context
 
 open Ftorlisp.OpTypes
 open Ftorlisp.UnTyAST
-open Std (HashMap)
+open Ftorlisp.Ty
+open Ftorlisp.Context
 
 namespace Ftorlisp.TyInference
-
-inductive Ty where
-  | int
-  | bool
-deriving Inhabited, Repr, BEq
-
-
-structure TyTable where
-  map : HashMap String Ty
-deriving Repr, BEq
-
-namespace TyTable
-  def init : TyTable :=
-    let map : HashMap String Ty := ∅
-    let full_map := map.insertMany [
-      ("Int", .int),
-      ("Bool", .bool)
-    ]
-    ⟨full_map⟩
-
-  def lookup (ty_table : TyTable) (name : String) : Option Ty :=
-    ty_table.map.get? name
-
-  def int (ty_table : TyTable) : Ty :=
-    (ty_table.lookup "Int").get!
-
-  def bool (ty_table : TyTable) : Ty :=
-    (ty_table.lookup "Bool").get!
-
-  def isInt (ty_table : TyTable) (ty : Ty) : Bool :=
-    ty_table.int == ty
-end TyTable
-
-structure VarTyEnv where
-  parent : Option VarTyEnv
-  scope : HashMap String Ty
-deriving Repr, BEq
-
-namespace VarTyEnv
-
-  def init : VarTyEnv :=
-    {parent := .none, scope := .emptyWithCapacity}
-
-  def lookup (env : VarTyEnv) (name : String) : Option Ty :=
-    env.scope.get? name
-
-  def insert (env : VarTyEnv) (name : String) (ty : Ty) : VarTyEnv :=
-    { env with scope := env.scope.insert name ty}
-
-end VarTyEnv
 
 mutual
   inductive TyASTExpr where
@@ -70,6 +22,7 @@ mutual
 
   inductive TyASTStmt where
     | let_stmt (ty : Ty) (name : String) (val : TyASTExpr)
+    | dec (ty : Ty) (name : String)
   deriving Repr, BEq
 
   inductive TyAST where
@@ -84,6 +37,9 @@ mutual
   | negNotNum (arg : TyASTExpr)
   | ifTypeMissmatch (then_ast : TyASTExpr) (else_ast : TyASTExpr)
   | ifConditionNotBool (test : TyASTExpr)
+  | unknownTy (ty_ast : UnTyASTTy)
+  | genericArgsNumMismatch (ty_ast : UnTyASTTy) (correct_num : Nat)
+  | genericFirstNotCons (ty_ast : UnTyASTTy)
   deriving Repr, BEq
 
 end
@@ -101,42 +57,36 @@ end TyASTExpr
 
 abbrev TyInfExcept := Except TyInfError
 
-
-inductive TyInfRes (α : Type) where
-  | envUpdate (env : VarTyEnv)
-  | ty (ty : α)
-deriving Repr, BEq
-
 mutual
-  private partial def expTyInference (exp : UnTyASTExpr) (ty_table : TyTable) (env : VarTyEnv) : TyInfExcept TyASTExpr :=
+  private partial def expTyInference (exp : UnTyASTExpr) (context : Context) : TyInfExcept TyASTExpr :=
     match exp with
-      | .intLit val => .ok $ .int ty_table.int val
-      | .bool val => .ok $ .bool ty_table.bool val
-      | .sym name => match (env.lookup name) with
+      | .intLit val => .ok $ .int context.tyInt val
+      | .bool val => .ok $ .bool context.tyBool val
+      | .sym name => match (context.varTyLookup name) with
         | .some ty => .ok $ .varRead ty name
         | .none => .error .undefinedVar
 
       | .binOp op arg1 arg2 => do
-        let arg1_ast ← expTyInference arg1 ty_table env
-        let arg2_ast ← expTyInference arg2 ty_table env
+        let arg1_ast ← expTyInference arg1 context
+        let arg2_ast ← expTyInference arg2 context
 
-        if arg1_ast.ty == arg2_ast.ty && arg1_ast.ty == ty_table.int then
+        if arg1_ast.ty == arg2_ast.ty && arg1_ast.ty == context.tyInt then
           return .binOp arg1_ast.ty op arg1_ast arg2_ast
         else
           .error $ .arithArgsTypeMismatch arg1_ast arg2_ast
 
       | .unOp .neg arg => do
-        let arg_ast ← expTyInference arg ty_table env
+        let arg_ast ← expTyInference arg context
 
-        if arg_ast.ty == ty_table.int then
+        if arg_ast.ty == context.tyInt then
           return (.unOp arg_ast.ty .neg arg_ast)
         else
           .error $ .negNotNum arg_ast
 
       | .if_expr test then_exp else_exp => do
-        let test_ast ← expTyInference test ty_table env
-        let then_ast ← expTyInference then_exp ty_table env
-        let else_ast ← expTyInference else_exp ty_table env
+        let test_ast ← expTyInference test context
+        let then_ast ← expTyInference then_exp context
+        let else_ast ← expTyInference else_exp context
 
         match test_ast.ty with
           | .bool =>
@@ -146,35 +96,59 @@ mutual
               .error $ .ifTypeMissmatch then_ast else_ast
           | _ => .error $ .ifConditionNotBool test_ast
 
-  private partial def stmtTyInference (stmt : UnTyASTStmt) (ty_table : TyTable) (env : VarTyEnv) : TyInfExcept TyASTStmt := do
+  private partial def tyTyInference
+    (ty_ast : UnTyASTTy) (context : Context) : TyInfExcept Ty := do
+    match ty_ast with
+      | .sym name => match context.tyLookup name with
+        | .some ty => return ty
+        | .none => .error $ .unknownTy ty_ast
+      | .call name arg_tys_asts =>
+        let opt_ty_cons := context.tyLookup name
+        match opt_ty_cons with
+          | .some ty_cons => match ty_cons with
+            | .generic_cons _name arg_tys_num => do
+              if arg_tys_asts.length == arg_tys_num then
+                let arg_tys ← arg_tys_asts.mapM (tyTyInference · context)
+                return .generic_spec ty_cons arg_tys
+              else
+                .error $ .genericArgsNumMismatch ty_ast arg_tys_num
+            | _ => .error $ .genericFirstNotCons ty_ast
+          | .none => .error $ .unknownTy ty_ast
+
+  private partial def stmtTyInference (stmt : UnTyASTStmt) (context : Context) : TyInfExcept TyASTStmt := do
     match stmt with
       | .let_stmt name val => do
-        let val_ast ← expTyInference val ty_table env
+        let val_ast ← expTyInference val context
         return (.let_stmt val_ast.ty name val_ast)
+      | .dec name arg_tys_asts ret_ty_ast => do
+        let args_tys ← arg_tys_asts.mapM (tyTyInference · context)
+        let ret_ty ← (tyTyInference ret_ty_ast context)
+        return .dec (.fn args_tys ret_ty) name
 
-  partial def astTyInference (ast : UnTyAST) (ty_table : TyTable) (env : VarTyEnv) : TyInfExcept TyAST := do
+
+  partial def astTyInference (ast : UnTyAST) (context : Context) : TyInfExcept TyAST := do
     match ast with
       | .expr exp => do
-        let exp_typed ← expTyInference exp ty_table env
+        let exp_typed ← expTyInference exp context
         return .exp exp_typed
       | .stmt stmt => do
-        let stmt_typed ← stmtTyInference stmt ty_table env
+        let stmt_typed ← stmtTyInference stmt context
         return .stmt stmt_typed
 end
 
 
-partial def programTyInference (ast_list : List UnTyAST) (ty_table : TyTable) (env : VarTyEnv) : TyInfExcept $ (List TyAST × VarTyEnv) := do
+partial def programTyInference (ast_list : List UnTyAST) (context : Context) : TyInfExcept $ (List TyAST × Context) := do
   match ast_list with
-    | [] => return ([], env)
+    | [] => return ([], context)
     | ast :: rest => do
-      let tyast ← astTyInference ast ty_table env
+      let tyast ← astTyInference ast context
       match tyast with
         | .exp _ => do
-          let (rest_tyast, rest_env) ← (programTyInference rest ty_table env)
+          let (rest_tyast, rest_env) ← (programTyInference rest context)
           return (tyast :: rest_tyast, rest_env)
         | .stmt stmt => do
           match stmt with
             | .let_stmt ty name _ => do
-              let new_env := env.insert name ty
-              let (rest_tyast, rest_env) ← (programTyInference rest ty_table new_env)
+              let new_context := context.varTyInsert name ty
+              let (rest_tyast, rest_env) ← (programTyInference rest new_context)
               return (tyast :: rest_tyast, rest_env)
