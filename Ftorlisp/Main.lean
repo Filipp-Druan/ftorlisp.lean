bcd34b9f3b81733a40596f8c817f29a58e7e9c70
Filipp Druan <druanf@bk.ru>
2026-07-14 17:52:14 +0300

    Переделал под Context

--- Ftorlisp/Main.lean
+++ Ftorlisp/Main.lean
@@ -1,10 +1,14 @@
 import Ftorlisp.FirstParser
 import Ftorlisp.SecondParser
 import Ftorlisp.TyInference
+import Ftorlisp.Context
+import Ftorlisp.TyAST
 
 open Ftorlisp.FirstParser
 open Ftorlisp.SecondParser
 open Ftorlisp.TyInference
+open Ftorlisp.Context
+open Ftorlisp.TyAST
 
 inductive GeneralError where
   | firstParserError (err :  FirstParserError)
@@ -12,15 +16,14 @@ inductive GeneralError where
   | tyInfError (err : TyInfError)
 deriving Repr
 
-partial def srcToTyAST (src : String) : Except GeneralError $ (List TyAST × VarTyEnv) := do
-  let env := VarTyEnv.init
-  let ty_table := TyTable.init
+partial def srcToTyAST (src : String) (context : Context) : Except GeneralError $ (List TyAST × Context) := do
 
   let pt ← programFirstParser src |> Except.mapError GeneralError.firstParserError
   let utast ← programSecondParser pt |> Except.mapError GeneralError.secondParserError
-  let tyast ← programTyInference utast ty_table env |> Except.mapError GeneralError.tyInfError
+  let tyast ← programTyInference utast context |> Except.mapError GeneralError.tyInfError
 
   return tyast
 
-#eval srcToTyAST "1 2 3"
-#eval srcToTyAST "(let num 5) (if true (* num num) 0)"
+#eval srcToTyAST "1 2 3" .init
+#eval srcToTyAST "(let num 5) (if true (* num num) 0)" .init
+#eval srcToTyAST "(dec foo [Int] Int)" .init
