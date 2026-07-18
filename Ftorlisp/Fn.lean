bb0677ff00b947933d9dd29808ef3003980b8585
Filipp Druan <druanf@bk.ru>
2026-07-18 18:09:21 +0300

    добавил make

--- Ftorlisp/Fn.lean
+++ Ftorlisp/Fn.lean
@@ -7,23 +7,31 @@ open Ftorlisp.TyAST
 namespace Ftorlisp.Fn
 
 structure FnDef where
-  ast : List TyAST
-deriving Repr, BEq
+  ast : TyASTStmt -- ast : TyASTStmt.def_stmt
+deriving Inhabited, Repr, BEq
 
 inductive FnError where
   | fnDefined
+  | incorrectArgsNum
+  | tyIsNotFn
 deriving Repr, BEq
 
 structure Fn where
   ty : Ty
   definition : Option FnDef
-deriving Repr, BEq
+deriving Inhabited, Repr, BEq
 
 namespace Fn
+  def make (ty : Ty) : Except FnError Fn :=
+    match ty with
+      | .fn _ _ => .ok ⟨ty, .none⟩
+      | _ => .error .tyIsNotFn
+
   def makeFromDecTy (dec_ty : Ty) : Fn :=
     ⟨dec_ty, .none⟩
 
   def addDef (fn : Fn) (fn_def : FnDef) : Except FnError Fn :=
+
     match fn.definition with
       | .some _ => .error .fnDefined
       | _ => .ok {fn with definition := .some fn_def}
